92b34532a4098e5ae6deeb072af12e56d040c039
Tim Gallant <me@timgallant.us>
2014-12-07 19:37:33 -0700

    adding to readme and adding yo-new function

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ##Introduction
 
-`eyo` is an Emacs library that caters to a modern frontend workflow.
+`yo.el` is an Emacs library that caters to a modern frontend workflow.
 Interface with yeoman, grunt, bower, and npm right inside of Emacs!
 
 ##Setup
@@ -8,13 +8,39 @@ Interface with yeoman, grunt, bower, and npm right inside of Emacs!
 `eyo` depends on yeoman, grunt, bower, and npm. Follow your distro's
 preferred method for installation.
 
-Debian/Ubuntu based:
+Debian/Ubuntu:
 
-    sudo apt-get install yo
+    sudo apt-get install npm
+    sudo npm install -g yo
+
+FreeBSD:
+
+    sudo portmaster www/npm
+    sudo npm install -g yo
 
 ##Example Usage
 
-Coming soon...
+Here is an example workflow for an AngularJS project
+
+Create new project with yeoman:
+
+    M-x yo-new <RET> /path/to/proj/ <RET> angular <RET>
+
+Start development server (current buffer must be file from project):
+
+    M-x grunt-server
+
+Add a new controller:
+
+    M-x yo <RET> angular:controller test <RET>
+
+Install new bower dependency to project:
+
+    M-x bower-install-save <RET> your_pkg_here <RET>
+
+Install new npm dependency to project:
+
+    M-x npm-install-save <RET> your_pkg_here <RET>
 
 ##Default Keybindings
 
@@ -23,6 +49,6 @@ Keybind   | Command
 `M-g s`   |  grunt-server
 `M-g b i` |  bower-install
 `M-g b s` |  bower-install-save
-`M-g n i` |  npm-install
-`M-g n s` |  npm-install-save
-`M-g n g` |  npm-install-g
+`M-g i i` |  npm-install
+`M-g i s` |  npm-install-save
+`M-g i g` |  npm-install-g
--- yo.el
+++ yo.el
@@ -1,4 +1,4 @@
-;;; eyo.el --- An Emacs library for a modern frontend workflow
+;;; yo.el --- An Emacs library for a modern frontend workflow
 
 ;; Copyright (c) 2014 Tim Gallant <me@timgallant.us>
 
@@ -31,9 +31,10 @@
 
 (require 'ansi-color)
 
-;; ansi filter for process output
+;; Functions
 
 (defun* ansi-filter (proc string)
+  "ansi filter for process output"
   (when (buffer-live-p (process-buffer proc))
     (with-current-buffer (process-buffer proc)
       (let ((moving (= (point) (process-mark proc))))
@@ -45,10 +46,11 @@
         (if moving (goto-char (process-mark proc)))))))
 
 (defun* locate-file (filename &optional (dir default-directory))
+  "recursively look for file in parent directories"
   (let ((has-gruntfile (directory-files dir nil (format "^%s$" filename)))
         (is-root (equal dir "/")))
     (cond
-     (has-gruntfile (format "%s/" dir))
+     (has-gruntfile (format "%s" dir))
      (is-root
       (print (format
               "No Gruntfile found in either %s or any parent directory!"
@@ -56,20 +58,20 @@
       nil)
      ((locate-file filename (expand-file-name ".." dir))))))
 
-(locate-file "bower.json" "/home/tim/projects/tor-calc/app/scripts/")
-
-;; Functions
-
 (defun grunt-cmd (cmd)
+  "wrapper for grunt"
+  (setq default-directory (locate-file "Gruntfile.js"))
+  (message default-directory)
   (set-process-filter
    (start-process-shell-command
     "grunt-log"
     "*grunt-log*"
-    "grunt ")
+    (format "grunt %s" cmd))
    'ansi-filter)
-  (switch-to-buffer-other-window "*grunt-server*"))
+  (switch-to-buffer-other-window "*grunt-log*"))
 
 (defun yo-cmd (cmd)
+  "wrapper for yo"
   (setq default-directory (locate-file "Gruntfile.js"))
   (message default-directory)
   (set-process-filter
@@ -81,6 +83,7 @@
   (switch-to-buffer-other-window "*yo-log*"))
 
 (defun bower-cmd (cmd)
+  "wrapper for bower"
   (setq default-directory (locate-file "bower.json"))
   (message default-directory)
   (set-process-filter
@@ -92,6 +95,7 @@
   (switch-to-buffer-other-window "*bower-log*"))
 
 (defun npm-cmd (cmd)
+  "wrapper for npm"
   (setq default-directory (locate-file "package.json"))
   (message default-directory)
   (set-process-filter
@@ -105,48 +109,68 @@
 ;; yeoman commands
 
 (defun yo (cmd)
+  "run a specified yo command"
   (interactive "SCommand: ")
   (yo-cmd cmd))
+
+(defun yo-new (wd cmd)
+  "create a new project"
+  (interactive "GProject Directory: \nSCommand: ")
+  (unless (file-exists-p wd)
+    (make-directory wd))
+  (setq default-directory wd)
+  (with-temp-buffer
+    (shell (current-buffer))
+    (process-send-string nil (format "yo %s" cmd))))
 
 ;; bower commands
 
 (defun bower (cmd)
+  "run a specified bower command"
   (interactive "SCommand: ")
   (bower-cmd cmd))
 
 (defun bower-install ()
+  "run bower install"
   (interactive)
   (bower-cmd "install"))
 
 (defun bower-install-save (pkg)
+  "install bower package and save it to your bower.json file"
   (interactive "SPackage: ")
   (bower-cmd (format "install %s --save")))
 
 ;; npm commands
 
 (defun npm (cmd)
+  "run a specified npm command"
   (interactive "SCommand: ")
   (npm-cmd cmd))
 
 (defun npm-install ()
+  "run npm install"
   (interactive)
   (npm-cmd "install"))
 
 (defun npm-install-save (pkg)
+  "install npm package and save it to your package.json file"
   (interactive "SPackage: ")
   (npm-cmd (format "install %s --save")))
 
 (defun npm-install-g (pkg)
+  "globally install npm package"
   (interactive "SPackage: ")
   (npm-cmd (format "install -g %s")))
 
 ;; grunt commands
 
 (defun grunt (cmd)
+  "run a specified grunt command"
   (interactive "SCommand: ")
   (grunt-cmd cmd))
 
 (defun grunt-server ()
+  "run grunt server"
   (interactive)
   (grunt-cmd "server"))
 
@@ -155,6 +179,6 @@
 (global-set-key (kbd "M-g s") 'grunt-server)
 (global-set-key (kbd "M-g b i") 'bower-install)
 (global-set-key (kbd "M-g b s") 'bower-install-save)
-(global-set-key (kbd "M-g n i") 'npm-install)
-(global-set-key (kbd "M-g n s") 'npm-install-save)
-(global-set-key (kbd "M-g n g") 'npm-install-g)
+(global-set-key (kbd "M-g i i") 'npm-install)
+(global-set-key (kbd "M-g i s") 'npm-install-save)
+(global-set-key (kbd "M-g i g") 'npm-install-g)
